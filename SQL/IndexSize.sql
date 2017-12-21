a8a8e93f2c1b101add8c71d76c9addb2d63e446f
shashisadasivan <shashi.sadasivan@gmail.com>
2017-12-21 20:45:14 +1300

    SQL - index size added
    index size added

---
+++ SQL/IndexSize.sql
@@ -0,0 +1,10 @@
+sp_spaceused
+-- Ensure a USE <databasename> statement has been executed first.
+SELECT i.[name] AS IndexName
+    ,SUM(s.[used_page_count]) * 8 AS IndexSizeKB
+FROM sys.dm_db_partition_stats AS s
+INNER JOIN sys.indexes AS i ON s.[object_id] = i.[object_id]
+    AND s.[index_id] = i.[index_id]
+GROUP BY i.[name]
+ORDER BY i.[name]
+GO 
