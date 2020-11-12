f4d9e04557e77188174d2348e261a9d1b6adc90f
shashi s <shashi.sadasivan@gmail.cm>
2020-11-13 09:08:15 +1300

    Create Remove-LabelFileDuplicates.ps1

---
+++ D365/Powershell/Remove-LabelFileDuplicates.ps1
@@ -0,0 +1,35 @@
+﻿function Remove-LabelFileDuplicates {
+    
+    param(
+        [parameter(Mandatory=$true)]$labelFile
+    )
+
+    Write-Host "started $labelFile"
+
+    [Collections.Generic.List[String]]$lines = [IO.File]::ReadAllLines($labelFile)
+    [Collections.Generic.List[String]]$linesUsed
+    $isLabel = $false
+    $isPrevLineRemoved = $false
+
+    foreach($line in $lines){
+        $isLabel = $line.StartsWith(" ;");
+
+        if($isLabel){
+            if($isPrevLineRemoved -eq $false) {
+                $linesUsed += $line
+            }
+            $isPrevLineRemoved = $false
+        } else {
+            if($linesUsed -contains $line){
+                $isPrevLineRemoved = $true
+            } else {
+                $linesUsed += $line
+                $isPrevLineRemoved = $false
+            }
+        }
+    }
+
+    [IO.File]::WriteAllLines($labelFile, $linesUsed)
+}
+
+Remove-LabelFileDuplicates "C:\LocalData\LocalDev\VSProjects\RemoveDuplicateLines\RemoveDuplicateLines\bin\Debug\netcoreapp3.1\DXC_Integria.en-AU.label.txt"
