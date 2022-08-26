26cc07bd4be20cb14fc9788df613eb651a20457f
Shashi <shashi.sadasivan@gmail.com>
2022-08-27 01:04:16 +1000

    Update DbToMultiUser.sql

--- SQL/DbToMultiUser.sql
+++ SQL/DbToMultiUser.sql
@@ -9,12 +9,13 @@ GO
 
 --Incase there are services to kill.
 -- Make sure to stop the Batch services
---SELECT db_name(resource_database_id) AS DB_Name, request_session_id
---FROM sys.dm_tran_locks l
---WHERE resource_type = 'DATABASE'
---  AND EXISTS (SELECT * FROM sys.dm_exec_sessions s 
---                WHERE l.request_session_id = s.session_id
---                  AND s.is_user_process = 1)
---  and resource_database_id = db_id('AXDB')
-
+/*
+SELECT db_name(resource_database_id) AS DB_Name, request_session_id
+FROM sys.dm_tran_locks l
+WHERE resource_type = 'DATABASE'
+  AND EXISTS (SELECT * FROM sys.dm_exec_sessions s 
+                WHERE l.request_session_id = s.session_id
+                  AND s.is_user_process = 1)
+  and resource_database_id = db_id('AXDB')
+*/
 --  KILL 57
