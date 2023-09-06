ba5cf87a0e391ae5783b24bed39dd6bddc4c38b0
Shashi <shashi.sadasivan@gmail.com>
2023-09-06 14:14:10 +1200

    Update envPrep.ps1

--- D365/Powershell/envPrep.ps1
+++ D365/Powershell/envPrep.ps1
@@ -1,7 +1,12 @@
 # list of exclusions to add to Microsoft defender to make things work "faster" as MsMpeng.exe takes up a lot of time each time the IIS server starts up and when the reporting services is initiated.
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/configure-extension-file-exclusions-microsoft-defender-antivirus?view=o365-worldwide
+# Get-MpPreference | Select-Object -ExpandProperty ExclusionPath
 
 # Add SQL sserver to the exclusion list
 Add-MpPreference -ExclusionPath "C:\Program Files\Microsoft SQL Server"
 
 Add-MpPreference -ExclusionPath "K:\AosService\PackagesLocalDirectory"
 Add-MpPreference -ExclusionPath "K:\AosService\WebRoot"
+Add-MpPreference -ExclusionPath '%userprofile%\Documents\IISExpress'
+Add-MpPreference -ExclusionPath '%userprofile%\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations'
+Add-MpPreference -ExclusionPath 'C:\Program Files\Common Files\Microsoft Shared\Office16\mso20win32client.dll'
